0d7a9835bc327a70efa17bc39f4f51788f0882f6
Victor Ung <vung@ucsd.edu>
2021-02-18 10:09:01 -0800

    finished lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,13 +1,16 @@
-'use strict';
+"use strict";
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
+$(document).ready(function () {
+    initializePage();
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+    // your code here
+    $(".likeBtn").click(() => {
+        ga("send", "event", "like", "click");
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var projects = require('../projects.json');
+var projects = require("../projects.json");
 
 /*
  * GET home page.
  */
 
-exports.view = function(request, response){
-  	response.render('index', projects);
+exports.view = function (request, response) {
+    projects["viewAlt"] = false;
+    response.render("index", projects);
+};
+
+exports.viewAlt = function (request, response) {
+    projects["viewAlt"] = true;
+    response.render("index", projects);
 };
